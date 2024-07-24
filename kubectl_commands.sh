d8baa91c128b4992ce108c99db948e780d38692d
Alex Pavlov <122205929+aipavlo@users.noreply.github.com>
2024-07-24 12:05:15 +0300

    Update kubectl_commands.sh

--- kubectl_commands.sh
+++ kubectl_commands.sh
@@ -3,3 +3,9 @@ kubectl get namespaces
 
 # get description of namespace
 kubectl describe namespace <namespace-name>
+
+kubectl cluster-info
+
+# check logs of pods
+kubectl get pods
+kubectl logs <pod_name>
