8a40da26e31a1c1aa6e577cce7b6b26b1e3787bf
Alex Pavlov <122205929+aipavlo@users.noreply.github.com>
2024-07-24 12:02:44 +0300

    Create helm.sh
    add helm.sh

---
+++ helm.sh
@@ -0,0 +1,9 @@
+# install helm chart
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+
+ helm version
+ helm repo update
+ helm repo add incubator https://charts.helm.sh/incubator --force-update
+ helm search repo <name>
