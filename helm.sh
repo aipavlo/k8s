f2c3c58e9b5dc2a710b21fad50e3784cc993bb57
Alex Pavlov <122205929+aipavlo@users.noreply.github.com>
2024-07-29 16:35:26 +0300

    Update helm.sh

--- helm.sh
+++ helm.sh
@@ -6,6 +6,7 @@ chmod 700 get_helm.sh
 helm version
 helm repo update
 helm repo add incubator https://charts.helm.sh/incubator --force-update
+helm repo add spark-operator https://kubeflow.github.io/spark-operator
 helm search repo <name>
 helm list
 helm repo list
