e2cb9f89d90cddb66144e360fb80071f6260595f
Alex Pavlov <122205929+aipavlo@users.noreply.github.com>
2024-07-30 13:38:34 +0300

    Update kubectl_commands.sh

--- kubectl_commands.sh
+++ kubectl_commands.sh
@@ -31,8 +31,6 @@ kubectl logs <pod_name>
 
 # get all deployments
 kubectl get deployments --all-namespaces
-
-
 kubectl get sparkapplications --namespace spark-operator
 kubectl delete sparkapplication spark-pi --namespace spark-operator
 kubectl delete pod <pod_name> --namespace=livy-operator
