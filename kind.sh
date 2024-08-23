152b4ea4973b02fef6f2b1c50478b18a993ac9eb
Alex Pavlov <122205929+aipavlo@users.noreply.github.com>
2024-08-23 13:58:05 +0300

    Update kind.sh

--- kind.sh
+++ kind.sh
@@ -6,3 +6,6 @@ kind get clusters
 
 # push image to Kubernetes internal repository
 kind load docker-image spark:our-own-apache-spark-kb8
+
+# push image to kubernetes repos
+kind load docker-image spark:version_tag
