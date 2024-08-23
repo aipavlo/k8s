ae1b760a08385dbabd43e429fb739384db69508d
Alex Pavlov <122205929+aipavlo@users.noreply.github.com>
2024-08-23 14:03:15 +0300

    Update kind.sh

--- kind.sh
+++ kind.sh
@@ -9,3 +9,9 @@ kind load docker-image spark:our-own-apache-spark-kb8
 
 # push image to kubernetes repos
 kind load docker-image spark:version_tag
+
+# create the service account named “spark”
+kubectl create serviceaccount spark
+
+# cluster-level role binding for the service account
+kubectl create clusterrolebinding spark-role - clusterrole=edit - serviceaccount=default:spark - namespace=default
